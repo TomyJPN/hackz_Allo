8e4104983ef9457d56d6c5c8205bcd4acf9b5d9b
kotarousakata <50367628+kotarousakata@users.noreply.github.com>
2019-06-23 03:46:10 +0900

    ホーム画面UI向上

--- TaskConnect/Assets/Scripts/ButtonController.cs
+++ TaskConnect/Assets/Scripts/ButtonController.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
     // Start is called before the first frame update
     public int buttonnumber;
+    public Sprite trueimage;
+    public Sprite falseimage;
+    private Image _image;
+    public bool OnClicked = false;
+    public MainButtonController MainButtonController;
+    
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void FalseButton()
+    {
+        _image.sprite = falseimage;
+    }
+
     public void OnClick()
     {
         string scene = SceneManager.GetActiveScene().name;
+        _image.sprite = trueimage;
+        OnClicked = true;
+        MainButtonController.ButtonClick(buttonnumber);
         switch (buttonnumber)
         {
             case 0:
@@ -36,6 +56,7 @@ public class ButtonController : MonoBehaviour
                 {
                     SceneManager.LoadScene("Loot box");
                 }
+                
                 break;
             case 4:
                 if (scene!="Mission")
