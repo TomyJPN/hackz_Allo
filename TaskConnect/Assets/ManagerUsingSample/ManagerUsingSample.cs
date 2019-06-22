d447ea8220a35b887dbc05b51188b51f2a3cda52
TomyJPN <masaya120331@gmail.com>
2019-06-22 17:44:56 +0900

    マネージャークラスと使用法サンプルコード作成

---
+++ TaskConnect/Assets/ManagerUsingSample/ManagerUsingSample.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 使い方解説
+/// </summary>
+public class ManagerUsingSample : MonoBehaviour {
+
+  void Start() {
+    //このスクリプト内で新規に作るタスクの宣言
+    Manager.Task myTask1 = new Manager.Task();
+    Manager.Task myTask2 = new Manager.Task();
+
+    int type = 1; //quest
+    string name = "課題";
+    string description = "教科書10ページ";
+    int difficully = 1;
+    DateTime deadLine= new DateTime(2000, 8, 1);
+    Manager.Instance.SetTask(type,name,description,difficully,deadLine);  //登録
+
+    type = 1; //quest
+    name = "掃除";
+    description = "自分の部屋";
+    difficully = 1;
+    deadLine = new DateTime(2000, 8, 2);
+    Manager.Instance.SetTask(type, name, description, difficully, deadLine); //登録
+
+
+    //取得
+    List<Manager.Task> list = new List<Manager.Task>();
+    list = Manager.Instance.GetTaskAll();
+    for(int i = 0; i < list.Count; i++) {
+      Debug.Log("タスク[" + i + "] 名前:" + list[i].Name + ", 説明:" + list[i].Description + ", GUID" + list[i].GUID);
+    }
+  }
+
+  void Update() {
+
+  }
+}
--- TaskConnect/Assets/Scripts/Manager.cs
+++ TaskConnect/Assets/Scripts/Manager.cs
@@ -1,18 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Manager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+/// <summary>
+/// ゲーム起動中常に単一で存在し続けるマネージャー(使い方：Manager.Instance.関数() )
+/// </summary>
+public class Manager : SingletonMonoBehaviour<Manager> {
+
+  public class Task {
+    /// <summary>
+    /// 一意のID
+    /// </summary>
+    public string GUID { get; set; }
+
+    /// <summary>
+    /// 1:"quest"，2:"mission"
+    /// </summary>
+    public int Type { get; set; }
+
+    /// <summary>
+    /// タスク名（例："数学課題"）
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 細かい内容（例：教科書10p 問題1）
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// 難易度（1,2,3）
+    /// </summary>
+    public int Difficulty { get; set; }
+
+    /// <summary>
+    /// 締め切り日
+    /// </summary>
+    public DateTime Deadline { get; set; }
+  }
+
+  private List<Task> taskList = new List<Task>();  //総合的に入れるリスト
+
+  /// <summary>
+  /// 新規のタスクをマネージャーへデータ追加します
+  /// </summary>
+  public void SetTask(int type,string name,string description,int difficulty,DateTime deadline) {
+    Task newTask = new Task();
+ 
+    newTask.GUID = Guid.NewGuid().ToString("N");
+    newTask.Type = type;
+    newTask.Name = name;
+    newTask.Description = description;
+    newTask.Difficulty = difficulty;
+    newTask.Deadline = deadline;
+
+    taskList.Add(newTask);
+  }
+
+  /// <summary>
+  /// 指定タイプでまとまったタスクリストを取得します
+  /// </summary>
+  /// <param name="type">"quest"，"mission"どちらか</param>
+  /// <returns></returns>
+  public List<Task> GetTaskListForType(int type) {
+    List<Task> returnTask = new List<Task>();
+
+    for(int i = 0; i < taskList.Count; i++) {
+      if (type == taskList[i].Type) {
+        returnTask.Add(taskList[i]);
+      }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    return returnTask;
+  }
+
+  /// <summary>
+  /// 全タスクを取得します
+  /// </summary>
+  /// <returns></returns>
+  public List<Task> GetTaskAll() {
+    return taskList;
+  }
+
+  /// <summary>
+  /// タスクを指定GUIDから削除します
+  /// </summary>
+  public void removeTask(string GUID) {
+    for (int i = 0; i < taskList.Count; i++) {
+      if (GUID == taskList[i].GUID) {
+        taskList.RemoveAt(i);
+      }
     }
+  }
+
 }
